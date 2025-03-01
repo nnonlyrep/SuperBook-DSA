7f4e5bbe7d0d6809380f928f2b5b778f3bb377e5
Aleexa Villafuerte <aVillafuerte@mcm.edu.ph>
2025-03-01 18:24:59 +0800

    equipments
    ui- ppAddEquip
    ui- ppEditEquip

---
+++ SuperBookFinalProj/GUI/PopUps/ppAdEquip.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SuperBookFinalProj.GUI.PopUps
+{
+    public partial class ppAdEquip : Form
+    {
+        public ppAdEquip()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SuperBookFinalProj/GUI/PopUps/ppAddEquip.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SuperBookFinalProj.GUI.PopUps
+{
+
+    public partial class ppAddEquip : Form
+    {
+        public ppAddEquip()
+        {
+            InitializeComponent();
+        }
+
+        private void ppAddEquip_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
